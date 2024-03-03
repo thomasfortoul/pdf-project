fa0a5f5f4bb287cb036b8fb39e05dfbaf83e1699
Thomas Fortoul <thomasfortoul0812@gmail.com>
2024-03-03 14:27:17 +1100

    dependency change

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
